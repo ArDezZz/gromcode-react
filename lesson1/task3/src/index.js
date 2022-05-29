2c3c008fbca5250ea07bd743423681c2363a9cf5
ArDezZz <aromudez@gmail.com>
2022-05-29 23:41:15 +0300

    fix1

--- lesson1/task3/src/index.js
+++ lesson1/task3/src/index.js
@@ -1,31 +1,13 @@
-// const renderGreeting = elem => {
-//   const containerElem = document.createElement('div');
-//   containerElem.classList.add('greeting');
-
-//   const titleElem = document.createElement('h1');
-//   titleElem.classList.add('greeting__title');
-//   titleElem.textContent = 'Hello, world!';
-
-//   const textElem = document.createElement('p');
-//   textElem.classList.add('greeting__text');
-//   textElem.textContent = 'I am learning React';
-
-//   containerElem.append(titleElem, textElem);
-
-//   elem.append(containerElem);
-// };
-
-// renderGreeting(rootElement);
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
+
 const rootElement = document.querySelector('#root');
 const element = (
-  <div className="greeting">
-    <h1 className="greeting__title">Hello, world!</h1>
-    <p className="greeting__text">I'am learning React</p>
+  <div className='greeting'>
+    <h1 className='greeting__title'>Hello, world!</h1>
+    <p className='greeting__text'>I'am learning React</p>
   </div>
 );
 
-//
 ReactDOM.render(element, rootElement);
