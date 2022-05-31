d6f38c78372fd8799358d3645bf4f39ab0f3f7b8
ArDezZz <aromudez@gmail.com>
2022-05-31 17:04:12 +0300

    lesson4-hw1

---
+++ lesson4/hw1/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react';
+import Clock from './Clock.jsx';
+
+const App = () => {
+  return (
+    <>
+      <Clock location="NEW YORK" offset={-5} />
+      <Clock location="KYIV" offset={2} />
+      <Clock location="LONDON" offset={0} />
+    </>
+  );
+};
+
+export default App;
