c591c5deca3e08a44833999c7c455cba930a3965
ArDezZz <aromudez@gmail.com>
2022-05-31 21:56:57 +0300

    hw1

---
+++ lesson5/hw1/src/Toggler.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Toggler extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: 'Off',
+    };
+  }
+
+  changeStatus = () => {
+    if (this.state.status === 'Off') {
+      this.setState({
+        status: 'On',
+      });
+    } else {
+      this.setState({
+        status: 'Off',
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="toggler" onClick={this.changeStatus}>
+        {this.state.status}
+      </div>
+    );
+  }
+}
+
+export default Toggler;
