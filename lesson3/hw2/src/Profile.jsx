0d648a471cad5ca646a5b5442b096ce743a3e53c
ArDezZz <aromudez@gmail.com>
2022-05-31 13:51:47 +0300

    fix2

--- lesson3/hw2/src/Profile.jsx
+++ lesson3/hw2/src/Profile.jsx
@@ -2,19 +2,15 @@ import React from 'react';
 import './index.scss';
 import moment from 'moment';
 
-const birthData = date => {
-  return moment(new Date(date)).format('DD MMM YY');
-};
-
 const Profile = props => {
   return (
     <>
       <div className="profile__name">
         {props.userData.firstName} {props.userData.lastName}
       </div>
-      <div className="profile__birth">{`Was born ${birthData(props.userData.birthDate)} in ${
-        props.userData.birthPlace
-      }`}</div>
+      <div className="profile__birth">{`Was born ${moment(props.userData.birthDate).format(
+        'DD MMM YY',
+      )} in ${props.userData.birthPlace}`}</div>
     </>
   );
 };
