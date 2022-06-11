d6ea236e12c4fdff204a13550ee87c406d81d4c2
ArDezZz <aromudez@gmail.com>
2022-06-11 16:13:45 +0300

    hw2

--- lesson9/hw2/src/index.jsx
+++ lesson9/hw2/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Search from './Search.jsx';
+import Game from './Game.jsx';
 
 const rootElem = document.querySelector('#root');
 
-ReactDOM.render(<Search />, rootElem);
+ReactDOM.render(<Game />, rootElem);
