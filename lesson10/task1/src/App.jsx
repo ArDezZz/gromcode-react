62a5dcec6a03df811771b23d24a5c9e074b2df25
ArDezZz <aromudez@gmail.com>
2022-06-13 16:36:39 +0300

    lesson10-task1

--- lesson10/task1/src/App.jsx
+++ lesson10/task1/src/App.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import UserProfile from './UserProfile.jsx';
+import UserMenu from './UserMenu';
 
-const App = () => {
-  return <div>Hello, React!</div>;
-};
+class Page extends Component {
+  state = {
+    userData: null,
+  };
 
-export default App;
+  componentDidMount() {
+    this.fetchUserData(this.props.userId);
+  }
+
+  fetchUserData = userId => {
+    fetch(`https://api.github.com/users/${userId}`)
+      .then(response => response.json())
+      .then(userData =>
+        this.setState({
+          userData,
+        }),
+      );
+  };
+  render() {
+    return (
+      <div className="page">
+        <header className="header">
+          <UserMenu userData={this.state.userData} />
+        </header>
+        <UserProfile userData={this.state.userData} />
+      </div>
+    );
+  }
+}
+export default Page;
