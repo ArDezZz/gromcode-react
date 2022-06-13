7603087eeba11ac90439aa55ebc9fa10b0efd71c
ArDezZz <aromudez@gmail.com>
2022-06-13 23:30:22 +0300

    fix

--- lesson10/task2/src/CartTitle.jsx
+++ lesson10/task2/src/CartTitle.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 
 const CartTitle = props => {
-  return (
-    <div className="cart-title">{`${props.userName.firstName}, you added ${props.count} items`}</div>
-  );
+  console.log(props.userName);
+  return <div className="cart-title">{`${props.userName}, you added ${props.count} items`}</div>;
 };
 
 export default CartTitle;
