2b1d3c43d23266d98c048e905a615c27b888ea2c
ArDezZz <aromudez@gmail.com>
2022-06-15 12:37:53 +0300

    push

--- lesson11/hw1/src/App.jsx
+++ lesson11/hw1/src/App.jsx
@@ -2,13 +2,10 @@ import React, { Component } from 'react';
 import Expand from './Expand';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return (
       <div className="app">
-        <Expand title={'Title'}>
+        <Expand>
           <p>
             Hooks are a new addition in React 16.8. They let you use state and other React features
             without writing a class.
--- lesson11/hw1/src/Expand.jsx
+++ lesson11/hw1/src/Expand.jsx
@@ -2,24 +2,23 @@ import React, { Component } from 'react';
 import 'font-awesome/css/font-awesome.min.css';
 
 class Expand extends Component {
-  constructor(props) {
-    super(props);
-  }
   state = {
     isOpen: false,
   };
-  showHandler = () => {
+
+  changeHandler = () => {
     this.setState({
       isOpen: !this.state.isOpen,
     });
   };
+
   render() {
     return (
       <div className="expand border">
         <div className="expand__header">
-          <span className="expand__title">{this.props.title}</span>
-          <button className="expand__toggle-btn" onClick={this.showHandler}>
-            <i className={`fa fa-solid fa-angle-${!this.state.isOpen ? 'down' : 'up'}`}></i>
+          <span className="expand__title">Some title</span>
+          <button className="expand__toggle-btn" onClick={this.changeHandler}>
+            <i className={`fa fa-solid fa-angle-${!this.state.isOpen ? `down` : `up`}`}></i>
           </button>
         </div>
         {!this.state.isOpen ? null : <div className="expand__content">{this.props.children}</div>}
@@ -27,5 +26,4 @@ class Expand extends Component {
     );
   }
 }
-
 export default Expand;
