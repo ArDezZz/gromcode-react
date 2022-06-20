2fc2b064f69fea717d8b52fde407cac1b69d8a7f
ArDezZz <aromudez@gmail.com>
2022-06-20 15:16:58 +0300

    some

--- lesson12/task/src/Task.jsx
+++ lesson12/task/src/Task.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
-const Task = ({ done, text, onChange, id, onDelete }) => {
+const Task = ({ id, done, text, onChange, onDelete }) => {
   const listItemClasses = classNames('list-item', { 'list-item_done': done });
-
   return (
     <li className={listItemClasses}>
       <input
-        className="list-item__checkbox"
         type="checkbox"
+        className="list-item__checkbox"
         defaultChecked={done}
         onChange={() => onChange(id)}
       />
