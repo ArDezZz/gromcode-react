607b386dd1f090e47703f9167ce72d494a109b80
ArDezZz <aromudez@gmail.com>
2022-06-21 15:16:05 +0300

    some rename file

--- lesson14/task3/src/index.jsx
+++ lesson14/task3/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Dimension from './Dimensions.jsx';
+import Dimension from './Dimension.jsx';
 
 const rootElement = document.querySelector('#root');
 
